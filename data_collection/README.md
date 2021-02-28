0061240581fc65bb3e527740edc1d9b805ab9d60
Philip <bizimisphilip@gmail.com>
2021-02-28 18:17:03 +0100

    Add data collecting script and documentation

--- data_collection/README.md
+++ data_collection/README.md
@@ -7,5 +7,18 @@ Screenshot resolution is 2048x2048. In the script, the numbers are different bec
 Every 300 websites, the browser will be closed and reopened. This helped to keep the script running for long periods of time.
 
 ```
+python -m pip install -U pip (21.0.1)
 pip install selenium
+pip install google-cloud-aiplatform
 ```
+
+Pipeline explanation:
+- define output folder paths
+- init chrome webdriver
+- traverse csv
+- every 300 websites, close driver and open a new window
+- open website
+- if website has a dominant color with over 85%, image is probably "bad" (go next)
+- use AutoML ai (trained on cookie images) to predict if image contains cookie windows or not
+- if both tests are successfull, save image
+- next website
--- data_collection/screenshots.py
+++ data_collection/screenshots.py
@@ -1,16 +1,47 @@
 import csv
-
+from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from predict_good_or_cookie import make_prediction
+import os
+import shutil
 
 line_count = 1
-start_at = 26286
+start_at = 50476
+
+output_path_good = "/mnt/d/new_images/good"
+output_path_bad = "/mnt/d/new_images/bad"
+output_path_temp = "/mnt/d/new_images/temp_before_prediction"
+output_path_screenshot = "/mnt/d/new_images/screenshots/"
+
+def get_most_used_color_percentage(im):
+    image_dict = {}
+
+    for pixel in im.getdata():
+        try:
+            image_dict[pixel] += 1
+        except:
+            image_dict[pixel] = 1
+
+        
+    max_key = max(image_dict, key=lambda key: image_dict[key])
+
+    highest_percentage = image_dict[max_key] / 4194304
+
+    return highest_percentage
+
+def downsample_image(im):
+    out = im.convert("RGB")
+    out = out.resize((512,512),Image.ANTIALIAS)
+    return out
+
 
 with open('tranco_GW9K.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
-    while line_count < 50000:
+    while line_count < 100000:
+
         chrome_options = Options()
         #chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=2064,2180")
@@ -18,20 +49,55 @@ with open('tranco_GW9K.csv') as csv_file:
         service.start()
         driver = webdriver.Remote(service.service_url, options=chrome_options)
 
-
         for row in csv_reader:
+            # find start_at website
             if line_count < start_at:
                 line_count += 1
                 continue
 
+            # break loop to open new driver (close and start up chrome)
             if line_count % 300 == 0:
                 break
 
+            # build path
             domain_str = str(row[1])
+            domain_file_name = domain_str.replace(".","_")+".png"
+
+            # final dataset folder
+            domain_output_path_good = os.path.join(output_path_good, domain_file_name)
+            # outsourced images (for control)
+            domain_output_path_bad = os.path.join(output_path_bad, domain_file_name)
+            # temporary image files for downsampling 512x512
+            domain_output_path_temp = os.path.join(output_path_temp, domain_file_name)
+            # inital screenshots 2048x2048
+            domain_output_path_screenshot = os.path.join(output_path_screenshot, domain_file_name)
+
             try:
+                # set timeout 4s
                 driver.set_page_load_timeout(4)
+                # get page
                 driver.get("https://" + domain_str)
-                driver.save_screenshot("/mnt/d/ScreenshotsThesis6/"+domain_str.replace(".","_")+".png")
+
+                driver.save_screenshot(domain_output_path_screenshot)
+
+                im = Image.open(domain_output_path_screenshot)
+
+                # check if image has a dominant color with over 85%
+                percentage = get_most_used_color_percentage(im)
+                if percentage > 0.85:
+                    im.save(domain_output_path_bad)
+                    continue
+
+                # check if the trained ai predicts a cookie screenshot
+                downsampled_image = downsample_image(im)
+                downsampled_image.save(domain_output_path_temp)
+
+                prediction = make_prediction(domain_output_path_temp)
+
+                if prediction == 0:
+                    shutil.move(domain_output_path_temp, domain_output_path_good)
+                elif prediction == 1:
+                    shutil.move(domain_output_path_temp, domain_output_path_bad)
             except:
                 print(line_count, "FAILED")
 
